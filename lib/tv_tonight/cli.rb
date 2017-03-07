047b1ebacce5d3de3c2998e0f109412390e12442
Jenna Francis <jef399@nyu.edu>
2017-03-07 19:55:47 +0000

    build out hard coded CLI

--- lib/tv_tonight/cli.rb
+++ lib/tv_tonight/cli.rb
@@ -15,8 +15,17 @@ class TvTonight::CLI
   end
 
   def more_info
-    puts "Which show would you like to learn more about?"
-    input = gets.strip
-    #returns series, time, channel, episode, description, rating, etc.
+    input = nil
+    while input != "exit"#make this loop. give option to return to list, error message if not valid
+      puts "Which show would you like to learn more about? Type list to return to the main list or exit to leave the program."
+      input = gets.strip
+      if input.to_i.between?(1,10)
+        puts "more tv show info"
+        #return series, time, channel, episode, description, rating, etc. on article number which equals input
+      else
+        puts "Not sure what you meant. Please try again."
+      end
+    end #while
   end
+
 end
