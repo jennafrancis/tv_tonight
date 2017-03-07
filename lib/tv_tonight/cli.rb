e087dc6f04635f8585a0ded9df97b776acdde45a
Jenna Francis <jef399@nyu.edu>
2017-03-07 19:18:59 +0000

    basic CLI setup

--- lib/tv_tonight/cli.rb
+++ lib/tv_tonight/cli.rb
@@ -5,6 +5,18 @@ class TvTonight::CLI
   end
 
   def call
-    puts "What's on TV Tonight:"
+    list_shows
+    more_info
+  end
+
+  def list_shows
+    puts "Top trending shows on tv tonight:"
+    #lists top 10 shows with series, time, and channel
+  end
+
+  def more_info
+    puts "Which show would you like to learn more about?"
+    input = gets.strip
+    #returns series, time, channel, episode, description, rating, etc.
   end
 end
