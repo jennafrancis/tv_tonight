2cbc70afc07e37353e83d2af2a946a98fabc42da
Jenna Francis <jef399@nyu.edu>
2017-03-09 19:26:37 +0000

    scrape_episodes method changed from hash to object attributes

--- lib/tv_tonight/episode.rb
+++ lib/tv_tonight/episode.rb
@@ -7,40 +7,28 @@ class TvTonight::Episode
   end
 
   def self.all
-    show_1 = self.new
-    show_1.name = "Supernatural"
-    show_1.time = "8:00 PM"
-    show_1.network = "NBC"
-
-    show_2 = self.new
-    show_2.name = "SYTYCD"
-    show_2.time = "9:00 PM"
-    show_2.network = "CBS"
-
-    [show_1,show_2]
+    @@all
   end
 
   def self.scrape_episodes
-    # episodes = []
 
     page = Nokogiri::HTML(open("http://www.tvguide.com/trending-tonight/"))
     binding.pry
     
     page.css(".listings-program-list").each do |show|
-      episode = {}
-      episode[:name] = show.css("h4").text
-      episode[:series] = show.css("h3").text
+      episode = self.new
+      episode.name = show.css("h4").text
+      episode.series = show.css("h3").text
       airing = show.css(".listings-program-airing-info")
       info = airing.split(" | ")
         air_time = info[0]
         air_network = info[1]
-      episode[:time] = air_time
-      episode[:network] = air_network
-      episode[:description] = show.css(".listings-program-description").text
-      episode[:url] = show.css("a").attr("href").text
-      episodes << episode
+      episode.time = air_time
+      episode.network = air_network
+      episode.description = show.css(".listings-program-description").text
+      episode.url = show.css("a").attr("href").text
+      @@all << episode
     end
-    episodes
   end
 
 end
