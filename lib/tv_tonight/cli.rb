f209ecd8d47563cc692c2c6885b5efdb156bfc97
Jenna Francis <jef399@nyu.edu>
2017-03-10 15:38:42 +0000

    CLI bug fixes

--- lib/tv_tonight/cli.rb
+++ lib/tv_tonight/cli.rb
@@ -12,29 +12,35 @@ class TvTonight::CLI
     puts "(As reported by TVGuide.com)"
     @shows = TvTonight::Episode.all
     @shows.each.with_index(1) {|show, index| puts "#{index}. #{show.series} - #{show.time} on #{show.network}"}
+    puts " "
   end
 
   def more_info
     input = nil
 
-    while input != "exit"#make this loop. give option to return to list, error message if not valid
+    while input != "exit"
       puts "Which show would you like more info about? Type list to return to the main list or exit to leave the program."
       input = gets.strip
 
-      if input.to_i.between?(1,25)
+      if input.to_i>0
         selected =  @shows[input.to_i - 1]
-        puts "#{selected.series} - #{selected.name}"
-        puts "#{selected.time} on #{selected.network}"
-        puts "#{selected.description}"
-        puts "For Cast, News, Photos and more about #{selected.series}, go to #{selected.url}."
-        #return series, time, channel, episode, description, rating, etc. on article number which equals input
+        if selected == nil
+          puts "Not sure what you meant. Please try again."
+        else
+          puts " "
+          puts "#{selected.series} - #{selected.name}"
+          puts "#{selected.time} on #{selected.network}"
+          puts "#{selected.description}"
+          puts "For Cast, News, Photos and more about #{selected.series}, go to #{selected.url}."
+          puts " "
+        end
       elsif input.downcase == "list"
         list_shows
       else
         puts "Not sure what you meant. Please try again."
       end
     end #while
-    
+
   end
 
   def thanks
